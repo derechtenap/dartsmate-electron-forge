b63f06be8e154dbe17fd78d74f68daea1478b4b9
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-01-15 22:59:16 +0100

    Add `react`

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -6,5 +7,6 @@ export default defineConfig({
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
     mainFields: ['module', 'jsnext:main', 'jsnext'],
+    plugins: [react()]
   },
 });
